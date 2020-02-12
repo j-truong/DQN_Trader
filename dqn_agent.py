9ba4b1e7e25005fbd5847435afaf72b77e0db490
j-truong <47543196+j-truong@users.noreply.github.com>
2020-02-12 17:16:18 +0000

    Update dqn_agent.py

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,12 +1,15 @@
+import tensorflow as tf
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, CuDNNLSTM, BatchNormalization, Dropout, LSTM
+#from keras.layers import Dense, CuDNNLSTM, BatchNormalization, Dropout, LSTM, Embedding
 from keras.optimizers import Adam
 
 import numpy as np
 from collections import deque
 import random
 import math
+
+from tensorflow.keras import layers
 
 min_replay_memory = 40
 replay_memory_size = 50
@@ -21,6 +24,7 @@ class dqn_agent:
 
 		self.model = self.create_model()
 
+	# Neural Network Model (Maybe RNN?)
 	def create_model(self):
 		model = Sequential([
 
@@ -36,20 +40,31 @@ class dqn_agent:
 			Dense(self.action_size, activation='linear')
 			])
 		model.compile(loss='mse', optimizer=Adam(lr=0.001), metrics = ['accuracy'])
+
+		#Simple RNN model:
+		#model = tf.keras.Sequential()
+		#model.add(layers.Embedding(input_dim=self.window, output_dim=3))
+		#model.add(layers.LSTM(128))
+		#model.add(layers.Dense(16))
+		#model.compile(loss='mse', optimizer='adam', metrics = ['accuracy'])
+
 		return model
 
+	# Action
 	def act(self,current_state,epsilon):
 		if np.random.random() < epsilon:
 			action = np.random.randint(0,self.action_size)
 		else:
 			action_list = self.model.predict(current_state)
 			action = np.argmax(action_list[0])
+
 		return action
 
+	# Normalise data
 	def normalise(self,data):
-		normalised_data = (data - data.min()) / (data.max() - data.min())
-		return np.array([normalised_data])
+		return (data - data.min()) / (data.max() - data.min())
 
+	# Train NN by replacing weights and applying Bellman's Equation
 	def train(self,batch_size):
 
 		if (len(self.memory) < min_replay_memory):
@@ -60,6 +75,7 @@ class dqn_agent:
 		for (current_state, action, reward, new_state, done) in minibatch:
 			
 			if not done:
+				# Bellman's Equation
 				target = reward + self.gamma * np.max(self.model.predict(new_state)[0])
 			else:
 				target = reward
@@ -67,4 +83,3 @@ class dqn_agent:
 			target_f = self.model.predict(current_state)
 			target_f[0][action] = target
 			self.model.fit(current_state, target_f, epochs=1, verbose=0)
-
